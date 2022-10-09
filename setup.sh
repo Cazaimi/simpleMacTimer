567d9bd98ad434a3cd067092ed8b32a94d0b1066
Anmol Shukla <anmol01shukla@gmail.com>
2022-10-09 15:34:45 -0400

    [Hacktoberfest22] Added the functionality to count up and count down (#9)

--- setup.sh
+++ setup.sh
@@ -2,8 +2,14 @@ echo "Enter your message"
 
 read message 
 
-echo $message > config.txt
+echo $message > message.txt
 
 clear
 
 echo You entered\: $message 
+
+echo "Do you want to count-up(0) or count-down(1)(default) to your input time?"
+
+read direction
+
+echo $direction > config.txt
